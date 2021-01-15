1b44f272db0a13c1a603bd5f9508d6cf4ee8bef7
viktoriia287 <75584040+viktoriia287@users.noreply.github.com>
2021-01-15 20:56:16 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long balance = 2_000_000_000;
+        long transfer_to = 500_000_000;
+        long new_balance = balance + transfer_to;
+        System.out.println(new_balance);
+    }
+}
